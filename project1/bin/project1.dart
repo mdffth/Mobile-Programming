3976cd796beaaf097397ffa721dcd78c9b53ecc0
daprett <athallahdaffa936@gmail.com>
2025-09-10 19:11:54 +0700

    comment

---
+++ project1/bin/project1.dart
@@ -0,0 +1,10 @@
+import 'package:project1/project1.dart' as project1;
+
+void main(List<String> arguments) {
+  // print('Hello world: ${project1.calculate()}!');
+  // Coment
+  /*multii line
+    multii line 
+    multii line 
+  */
+}
