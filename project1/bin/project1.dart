66218cc9bfc759508c36bf3e680c4d3b62371c42
daprett <athallahdaffa936@gmail.com>
2025-09-10 19:32:41 +0700

    Variabel

--- project1/bin/project1.dart
+++ project1/bin/project1.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:project1/project1.dart' as project1;
 const String nama = 'Fairuz';
 void main(List<String> arguments) {
@@ -10,4 +12,18 @@ void main(List<String> arguments) {
   // Final dan Const
   final String nama1 = 'Daffa';
   print("Nama saya $nama $nama1");
+
+  // Variabel
+  int umur = 20;
+  double tinggi = 173.5;
+  bool jenis_kelamin = true;
+  String alamat = "Jl.Candi VB";
+
+  stdout.writeln("Umur saya $umur tahun");
+  stdout.writeln("Tinggi saya $tinggi cm");
+  stdout.writeln("Jenis Kelamin ${jenis_kelamin ? "Laki-Laki" : "Perempuan"}");
+  stdout.writeln("Alamat saya $alamat");
+
+
+
 }
