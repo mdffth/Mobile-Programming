aea479d30ee4a9314845c0ac3884725a65bf52b3
daprett <athallahdaffa936@gmail.com>
2025-09-16 04:09:02 +0700

    if dan tenary

--- pertemuan3/bin/pertemuan3.dart
+++ pertemuan3/bin/pertemuan3.dart
@@ -1,5 +1,35 @@
+import 'dart:io';
+
 import 'package:pertemuan3/pertemuan3.dart' as pertemuan3;
 
 void main(List<String> arguments) {
-  print('Hello world: ${pertemuan3.calculate()}!');
+  // print('Hello world: ${pertemuan3.calculate()}!');
+
+  // int nilai = 75;
+  stdout.write("Masukkan nilai : "); 
+  String? input = stdin.readLineSync();  // Membaca inputan
+  int nilai = int.tryParse(input??'') ??0; //Konversi input menjadi int, jika input null nilai defaultnya int = 0 
+  if((nilai >= 75) && (nilai <=100)){
+    print("Nilai A");
+  } else if ((nilai >= 60) && (nilai <=74)){
+    print("Nilai B");
+  } else if ((nilai >=0) && (nilai <=59)){
+    print("Nilai C");
+  } else {
+    print("Maaf, Nilai yang anda masukkan tidak benar!!");
+  }
+
+  // if biasa
+
+  // String status;
+  // if(nilai >=60){
+  //   status = "Anda Lulus";
+  // } else{
+  //   status= "Maaf, anda belum lulus";
+  // }
+  // print(status);
+
+  // Menggunakan tenary
+  String status1 = (nilai>=60)? "Selamat, anda lulus" : "Maaf, anda belum lulus";
+  print(status1);
 }
