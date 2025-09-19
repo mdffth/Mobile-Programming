a601e5a6d00a1054b340c2595892b1504eec7e7d
daprett <athallahdaffa936@gmail.com>
2025-09-19 11:21:46 +0700

    List

---
+++ project2/bin/project2.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+import 'package:project2/project2.dart' as project2;
+
+void main(List<String> arguments) {
+  // print('Hello world: ${project2.calculate()}!');
+  var fl= List<int>.filled(4,1);
+  fl[0] = 1;
+  fl[1] = 2;
+  fl[2] = 3;
+  fl[3] = 4;
+  fl[4] = 5;
+
+  stdout.writeln(fl);
+
+
+}
