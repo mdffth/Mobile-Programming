093321bb5744dd3e54b09607d766489e45f8b132
daprett <athallahdaffa936@gmail.com>
2025-09-19 13:34:09 +0700

    input, for

--- project2/bin/project2.dart
+++ project2/bin/project2.dart
@@ -3,15 +3,38 @@ import 'dart:io';
 import 'package:project2/project2.dart' as project2;
 
 void main(List<String> arguments) {
-  // print('Hello world: ${project2.calculate()}!');
-  var fl= List<int>.filled(4,1);
-  fl[0] = 1;
-  fl[1] = 2;
-  fl[2] = 3;
-  fl[3] = 4;
-  fl[4] = 5;
+  // // print('Hello world: ${project2.calculate()}!');
+  // var fl= List<int>.filled(4,1);
+  // fl[0] = 1;
+  // fl[1] = 2;
+  // fl[2] = 3;
+  // fl[3] = 4;
 
-  stdout.writeln(fl);
+  // stdout.writeln(fl);
 
+  // var gl = [1,2,3];
+  // gl.add(4);
+  // gl.remove(2);
+
+  // stdout.writeln(gl);
+
+  var setnilai = <int>{};
+  // var setnilai2 = {3,3,4,5};
+
+  // print(setnilai2);
+  // print(setnilai.union(setnilai2));
+  // print(setnilai.intersection(setnilai2));
+
+  stdout.writeln("Massukkan jumlah nilai : ");
+  String? nilai = stdin.readLineSync();
+  int jumlah = int.tryParse(nilai ?? '')??0;
+  for (var i = 0; i < jumlah; i++) {
+    stdout.writeln("Mauskkan nilai ke-${i+1}: ");
+    String? angka = stdin.readLineSync();
+    int ank = int.tryParse(angka ?? '')??0;
+    setnilai.add(ank);
+  }
+
+  print(setnilai);
 
 }
