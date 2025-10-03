019247008444fccf4d6bb467556ce189f0fc7164
daprett <athallahdaffa936@gmail.com>
2025-10-03 13:01:16 +0700

    drawer

--- pertemuan6/lib/main.dart
+++ pertemuan6/lib/main.dart
@@ -24,6 +24,23 @@ class MyApp extends StatelessWidget {
           onPressed: () {},
           child: const Icon(Icons.add),
         ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: const <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(color: Colors.blue),
+                child: Text(
+                  'Header Drawer',
+                  style: TextStyle(color: Colors.white, fontSize: 24,),
+                ),
+              ),
+              ListTile(leading: Icon(Icons.message),title: Text('Messages')),
+              ListTile(leading: Icon(Icons.account_circle), title: Text('Profile')),
+              ListTile(leading: Icon(Icons.settings), title: Text('Settings')),
+            ],
+          ),
+        ),
       ),
     );
   }
