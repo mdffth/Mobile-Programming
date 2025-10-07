38f8efc5f0d41171071091a2485f78d9782ab0b0
daprett <athallahdaffa936@gmail.com>
2025-10-07 22:03:24 +0700

    Nav Drawer

--- tugaspertemuan6/lib/counter.dart
+++ tugaspertemuan6/lib/counter.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
+import 'main.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class counterApp extends StatefulWidget {
+  const counterApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<counterApp> createState() => _counterApp();
 }
 
 class Counter {
@@ -26,65 +23,98 @@ class Counter {
   }
 }
 
-class _MyAppState extends State<MyApp> {
+class _counterApp extends State<counterApp> {
   final Counter counter = Counter();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Counter App')),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        counter.decrement();
-                      });
-                    },
-                    child: Text('-'),
+    return Scaffold(
+      appBar: AppBar(title: Text('Counter App')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      counter.decrement();
+                    });
+                  },
+                  child: Text('-'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Text(
+                    '${counter.value}',
+                    style: TextStyle(fontSize: 24),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Text(
-                      '${counter.value}',
-                      style: TextStyle(fontSize: 24),
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        counter.increment();
-                      });
-                    },
-                    child: Text('+'),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-              OutlinedButton(
-                onPressed: () {
-                  setState(() {
-                    counter.reset();
-                  });
-                },
-                child: Text('Reset'),
-              ),
-            ],
-          ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      counter.increment();
+                    });
+                  },
+                  child: Text('+'),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            OutlinedButton(
+              onPressed: () {
+                setState(() {
+                  counter.reset();
+                });
+              },
+              child: Text('Reset'),
+            ),
+          ],
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              counter.increment();
-            });
-          },
-          child: Icon(Icons.add),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            counter.increment();
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Text(
+                'Menu Drawer',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Profile'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyApp()),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.calculate),
+              title: const Text('Counter App'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(
+                  context,
+                ).push(MaterialPageRoute(builder: (context) => counterApp()));
+              },
+            ),
+          ],
         ),
       ),
     );
--- tugaspertemuan6/lib/main.dart
+++ tugaspertemuan6/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'counter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,60 +13,108 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Profil Mahasiswa',
-            style: TextStyle(color: Colors.white),
-          ),
-          backgroundColor: Colors.blue,
-          leading: const FlutterLogo(
-            size: 20,
+      home: const ProfilePage(),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Profil Mahasiswa',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+        // leading: const FlutterLogo(
+        //   size: 20,
+        // ),
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ClipOval(
+                child: Image.asset(
+                  'assets/img/profile.jpg',
+                  width: 150,
+                  height: 150,
+                  fit: BoxFit.cover,
+                  errorBuilder: (context, error, stackTrace) {
+                    // sebagai placeholder
+                    return Container(
+                      width: 150,
+                      height: 150,
+                      color: Colors.grey[300],
+                      child: const Icon(
+                        Icons.error,
+                        color: Colors.red,
+                        size: 50,
+                      ),
+                    );
+                  },
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Nama : Muhammad Fairuz Daffa Athallah\nNIM : 2341760079\n Jurusan : Teknologi Informasi\n Kelas : SIB-3B\nAlamat : Jl.Kebenaran No 10, Malang\nHobi : Berenang',
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.email),
+                  SizedBox(width: 30),
+                  Icon(Icons.call),
+                  SizedBox(width: 30),
+                  Icon(Icons.facebook),
+                ],
+              ),
+            ],
           ),
         ),
-        body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ClipOval(
-                  child: Image.asset(
-                    'assets/img/profile.jpg',
-                    width: 150,
-                    height: 150,
-                    fit: BoxFit.cover,
-                    errorBuilder: (context, error, stackTrace){// sebagai placeholder
-                      return Container(
-                        width: 150,
-                        height: 150,
-                        color: Colors.grey[300],
-                        child: const Icon(Icons.error, color: Colors.red, size: 50),
-                      );
-                    },
-                  ),
-                ),
-                const SizedBox(height: 20),
-                const Text(
-                  'Nama : Muhammad Fairuz Daffa Athallah\nNIM : 2341760079\n Jurusan : Teknologi Informasi\n Kelas : SIB-3B\nAlamat : Jl.Kebenaran No 10, Malang\nHobi : Berenang',
-                  style: TextStyle(fontSize: 18),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const[
-                    Icon(Icons.email),
-                    SizedBox(width: 30),
-                    Icon(Icons.call),
-                    SizedBox(width: 30),
-                    Icon(Icons.facebook),
-                  ],
-                )
-              ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Text(
+                'Menu Drawer',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
             ),
-          ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Profile'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(
+                  context,
+                ).push(MaterialPageRoute(builder: (context) => MyApp()));
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.calculate),
+              title: const Text('Counter App'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(
+                  context,
+                ).push(MaterialPageRoute(builder: (context) => counterApp()));
+              },
+            ),
+          ],
         ),
       ),
     );
